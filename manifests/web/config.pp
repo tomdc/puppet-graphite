a4e27138ebb3790453c270013672ac15ba756db2
Tom De Cooman <tom.decooman@gmail.com>
2013-02-07 12:42:35 +0100

    web pkg needed first

--- manifests/web/config.pp
+++ manifests/web/config.pp
@@ -13,6 +13,8 @@
 # [Remember: No empty lines between comments and class definition]
 class graphite::web::config ($time_zone = $graphite::time_zone){
 
+  require graphite::params
+
   file {'local_settings.py':
     ensure    => file,
     path      => '/etc/graphite-web/local_settings.py',
@@ -20,6 +22,7 @@ class graphite::web::config ($time_zone = $graphite::time_zone){
     group     => 'root',
     mode      => '0644',
     notify    => Service['httpd'],
-    content   => template('graphite/local_settings.py.erb');
+    content   => template('graphite/local_settings.py.erb'),
+    require   => Package[$graphite::params::graphiteweb];
   }
 }
